3f3f5d0a92c5b2b8268a4c8ae0d438d2e282c943
Mads Hougesen <madshougesen@gmail.com>
2023-03-17 23:26:14 +0100

    feat: implement cli for converting (#5)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-export * from './typescript';
+import fs from 'fs/promises';
+import { format } from 'prettier';
+
+import { Command } from '@commander-js/extra-typings';
+
+import { handleParseToTS } from './typescript';
+
+const program = new Command();
+
+program.name('json2struct').description('CLI for converting JSON to TypeScript types').version('0.0.1');
+
+program
+    .command('convert', { isDefault: true })
+    .description('Convert JSON file to type file')
+    .argument('<file-path>')
+    .action(async (path) => {
+        const fileContent = await fs.readFile(path);
+
+        const json = JSON.parse(fileContent.toString());
+
+        const parsedStruct = handleParseToTS(json);
+
+        process.stdout.write(format(parsedStruct, { parser: 'typescript' }));
+    });
+
+program.addHelpCommand();
+
+program.parse();

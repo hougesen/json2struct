2e6b480ec5346cd9426d664b02734fc2c4dee265
Mads Hougesen <mads@mhouge.dk>
2023-03-18 01:03:15 +0100

    refactor(cli): cleaned up args

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,45 @@
 import fs from 'fs/promises';
 import { format } from 'prettier';
 
-import { Command } from '@commander-js/extra-typings';
+import { Command, Option } from '@commander-js/extra-typings';
 
 import { handleParseToTS } from './typescript';
 
 const program = new Command();
 
-program.name('json2struct').description('CLI for converting JSON to TypeScript types').version('0.0.1');
+program
+    .name('json2struct')
+    .description('CLI for converting JSON to TypeScript types')
+    .version('0.0.1')
+    .configureOutput({
+        writeOut: (str) => process.stdout.write(`[OUT] ${str}`),
+        writeErr: (str) => process.stdout.write(`[ERR] ${str}`),
+        outputError: (str, write) => write(`\x1b[31m${str}\x1b[0m`),
+    });
 
 program
-    .command('convert', { isDefault: true })
+    .command('convert <input> [output]', { isDefault: true })
     .description('Convert JSON file to type file')
-    .argument('<input-file>')
-    .argument('[output-file]', '')
-    .action(async (inputPath, outputPath) => {
+    .option('--disable-format')
+    .addOption(new Option('-lang, --language <output-language>').choices(['typescript']).default('typescript'))
+    .action(async (inputPath, outputPath, args) => {
+        console.info(`\u001b[32mjson2struct: Converting ${inputPath} to ${args.language}:\u001b[0m`);
+
         const fileContent = await fs.readFile(inputPath);
 
         const json = JSON.parse(fileContent.toString());
 
-        const parsedStruct = handleParseToTS(json);
+        if (args.language === 'typescript') {
+            const parsedStruct = handleParseToTS(json);
 
-        const formattedStruct = format(parsedStruct, { parser: 'typescript' });
+            const formattedStruct = args?.disableFormat ? parsedStruct : format(parsedStruct, { parser: 'typescript' });
 
-        if (outputPath?.length) {
-            await fs.appendFile(outputPath, formattedStruct);
+            if (outputPath?.length) {
+                await fs.appendFile(outputPath, formattedStruct);
+            }
+
+            console.info(formattedStruct);
         }
-
-        process.stdout.write(formattedStruct);
     });
 
 program.addHelpCommand();

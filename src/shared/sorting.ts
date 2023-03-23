6f8b1fd7bf8e057679bdcdd6e14c87d666ce213f
Mads Hougesen <madshougesen@gmail.com>
2023-03-23 18:44:14 +0100

    feat(tokenizer): implement a basic tokenizer (#13)
    * test(tokenizer): base test for the tokenizer
    
    * feat(tokenizer): implement basic tokenizer
    
    * chore: bump version to 0.1.1

--- src/shared/sorting.ts
+++ src/shared/sorting.ts
@@ -1,3 +1,5 @@
+import { Token } from '../tokenizer';
+
 export function keySort(a: string, b: string) {
     return a < b ? -1 : 1;
 }
@@ -6,4 +8,12 @@ export function containerSort(a: string, b: string): number {
     if (a?.length === b.length) return keySort(a, b);
 
     return a.length < b.length ? -1 : 1;
+}
+
+export function sortChildren(a: Token, b: Token): 1 | -1 {
+    if (a?.key && b?.key && a?.key !== b?.key) {
+        return a.key < b.key ? -1 : 1;
+    }
+
+    return a.type < b.type ? -1 : 1;
 }
